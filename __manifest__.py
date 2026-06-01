e7287154cdcf0e1b0a4e90bc89feb8fc120627eb
Ilyasselkh <109107947+Ilyasselkh@users.noreply.github.com>
2026-06-01 11:53:28 +0100

    Add __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Article SAP",
+    'version': '1.0.1',
+    'category': 'ArticleSAP',
+    'sequence' : -100,
+    'application' : True,
+    'installable': True,
+    'license': 'LGPL-3',
+    'depends': ['base','mail'],    
+    'data':[
+        'data/mail_template_data.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/article.xml',
+        'views/derogation.xml',
+        'views/deleted.xml',
+    ]
+
+}
